ac01071f3a71dd6c5c9805e0c5bc9f8afd8330d4
Cosmin Stoian <cosmyn9708@gmail.com>
2020-11-22 14:26:33 +0200

    Minor change.

--- src/main/kotlin/tech/sharply/trueprice/api/persistence/models/platforms/PlatformProductOffer.kt
+++ src/main/kotlin/tech/sharply/trueprice/api/persistence/models/platforms/PlatformProductOffer.kt
@@ -1,6 +1,13 @@
 package tech.sharply.trueprice.api.persistence.models.platforms
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
+import tech.sharply.trueprice.api.persistence.models.Product
 
 @Document(collection = "_platforms.offers")
-class PlatformProductOffer ()
+class PlatformProductOffer(
+        @Id var id: String? = null,
+        @DBRef var product: Product? = null,
+        @DBRef var platform: Platform? = null
+)
